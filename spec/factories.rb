f3b9777da89effe0ca6dbbdc53469d4f9e4cb122
Yu Jiang Tham <ytham@apple.com>
2013-02-26 00:33:59 -0800

    Finish user edit, update, index, and destroy

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,13 @@
 FactoryGirl.define do
   factory :user do
-    name	"YJT"
-    email	"yj@t.com"
+    sequence(:name)		{ |n| "Person #{n}" }
+    sequence(:email)	{ |n| "person_#{n}@example.com" }
     password	"foobar"
     password_confirmation	"foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
 
